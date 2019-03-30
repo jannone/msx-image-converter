b7e9f954dc071c6c6015cd576a4373f5c631f2c9
Rafael Jannone <jannone@spotwish.com>
2019-03-30 19:13:04 +0000

    Add test for CIEDE2000

---
+++ tests/ciede.spec.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs"
+
+import { ciede2000Lab } from "../src/ciede"
+
+const roundFloat = (v) => Math.round(parseFloat(v) * 10000) / 10000
+
+const givenCIEDEdata = () => {
+  const data = fs.readFileSync("./tests/fixtures/ciede2000testdata.txt", "utf8")
+  return data.split("\n").map((line) => {
+    const values: number[] = line.trim().split("\t").map(parseFloat)
+    return values
+  })
+}
+
+describe("CIEDE2000", () => {
+
+  givenCIEDEdata().forEach(([ L1, a1, b1, L2, a2, b2, dist ]) => {
+    it(`distance must be ${dist}`, () => {
+      const result = ciede2000Lab(L1, a1, b1, L2, a2, b2)      
+      expect(roundFloat(result)).toBe(dist)
+    })
+  })
+
+})
+
