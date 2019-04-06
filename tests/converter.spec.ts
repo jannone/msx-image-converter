f0264ceadb1f768f11639c89686d733ab3d6def3
Rafael Jannone <jannone@spotwish.com>
2019-04-06 23:12:42 +0100

    Improve converter test

--- tests/converter.spec.ts
+++ tests/converter.spec.ts
@@ -13,15 +13,15 @@ const givenAnImage = (): Image => {
 
 describe("Converter", () => {
   it("must convert an image", () => {
-    const imageStorage: ImageStorageAdapter = {
-      readImage: (filename: string) => givenAnImage(),
-      saveImage: (image: Image, filename: string) => null
+    const imageStorage = {
+      readImage: jest.fn( (filename: string) => givenAnImage() ),
+      saveImage: jest.fn( (image: Image, filename: string) => null )
     }
-    const imageFunctions: ImageFunctionsAdapter = {
-      createImage: (width: number, height: number, color?: RGBColor) => givenAnImage(),
-      contrast: (inputImage: Image, outputImage: Image, amount: number) => null,
-      sharpen: (inputImage: Image, outputImage: Image, hard: boolean) => null,
-      quantize: (image1: Image, image2: Image, palette: Palette) => null
+    const imageFunctions = {
+      createImage: jest.fn( (width: number, height: number, color?: RGBColor) => givenAnImage() ),
+      contrast: jest.fn( (inputImage: Image, outputImage: Image, amount: number) => null ),
+      sharpen: jest.fn( (inputImage: Image, outputImage: Image, hard: boolean) => null ),
+      quantize: jest.fn( (image1: Image, image2: Image, palette: Palette) => null )
     }
     const options: ConversionOptions = {
       palette: [ {r: 0, g: 0, b: 0} ],
@@ -29,5 +29,15 @@ describe("Converter", () => {
     }
     const convertImage = buildImageConverter(imageStorage, imageFunctions)
     convertImage("input.png", "output.png", options)
+
+    expect(imageStorage.readImage.mock.calls.length).toBe(1)
+    expect(imageStorage.readImage.mock.calls[0][0]).toBe("input.png")
+    expect(imageStorage.saveImage.mock.calls.length).toBe(1)
+    expect(imageStorage.saveImage.mock.calls[0][1]).toBe("output.png")
+
+    expect(imageFunctions.createImage.mock.calls.length).toBe(1)
+    expect(imageFunctions.contrast.mock.calls.length).toBe(1)
+    expect(imageFunctions.sharpen.mock.calls.length).toBe(1)
+    expect(imageFunctions.quantize.mock.calls.length).toBe(1)
   })
 })
